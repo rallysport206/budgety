36adb9abea165201969e88822d3992596b3d9c2e
rallysport206 <phan.tony125@gmail.com>
2020-05-18 19:09:47 -0700

    reading data input

--- starter/app.js
+++ starter/app.js
@@ -1,27 +1,56 @@
+//BUDGET CONTROLLER
 let budgetController = (function(){
-    let x = 23;
-    let add = function(a) {
-        return x + a;
-    }
-    return{
-        publicTest: function(b) {
-            return add(b)
-        }
-    }
+
 })();
 
+//UI CONTROLLER
 let UIController = (function(){
 
-})();
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
 
-let controller =(function(budgetCtrl, UICtl){
-
-    let z =budgetCtrl.publicTest(5);
     return {
-        anotherPublic: function(){
-            console.log(z);
+        getInput: function() {
+            return{
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function(){
+            return DOMstrings;
         }
     }
+})();
 
+//GLOBAL APP CONTROLLER
+let controller =(function(budgetCtrl, UICtl){
+    let DOM = UICtl.getDOMstrings();
+
+    const ctrlAddItem = function(){
+
+        // 1 get filed input data
+        let input = UICtl.getInput();
+        console.log(input);
+        // 2 add the item to budget controller
+        // 3 add the item to the UI
+        // 4 calculate the budget
+        // display the budget on the UI
+
+    
+    }
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+
+    });
 
 })(budgetController, UIController);
+
