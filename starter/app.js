0d575e1a4c447e67aa43ad95f635da6b536533f4
rallysport206 <phan.tony125@gmail.com>
2020-05-19 19:08:04 -0700

    add new items

--- starter/app.js
+++ starter/app.js
@@ -24,6 +24,36 @@ let budgetController = (function(){
         }
     };
 
+    return {
+        addItem: function(type, des, val) {
+            let newItem, ID;
+            
+            //create new ID
+            if(data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length -1].id + 1;
+            } else {
+                ID = 0;
+            }   
+
+            //create new item based on 'inc' or 'exp type
+            if (type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            }   else if (type === 'inc') {
+                newItem = new Income(ID, des, val );
+            }
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            //return the new element
+            return newItem;
+
+        },
+        testing: function() {
+            console.log(data);
+            
+        }
+    };
+
 })();
 
 //UI CONTROLLER
@@ -67,10 +97,11 @@ let controller =(function(budgetCtrl, UICtl){
 
 
     const ctrlAddItem = function(){
-
+        let input, newItem;
         // 1 get filed input data
-        let input = UICtl.getInput();
+        input = UICtl.getInput();
         // 2 add the item to budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3 add the item to the UI
         // 4 calculate the budget
         // display the budget on the UI
